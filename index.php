222cf6f9a20e05cd6517ed8ef5abb01b6ea1d0c7
arhamlb17 <arhamlamabelawa3@gmail.com>
2025-08-12 08:31:50 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -10,6 +10,11 @@
 <body>
     <h1>Hello world!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel tenetur, praesentium quas dignissimos sit soluta repudiandae aspernatur illo maxime reiciendis neque nobis culpa mollitia nisi quis nemo recusandae magni tempore?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem consectetur tempore harum nulla quas ea nam numquam, eveniet beatae ducimus consequuntur, dignissimos dolorum veritatis ut, animi eum. Minima, minus amet.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 
 </html>

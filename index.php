c218d87f0c0795b7fda3f15b94ea137a7785181c
arhamlb17 <arhamlamabelawa3@gmail.com>
2025-08-12 08:24:43 +0700

    menambahkan paragraf

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Git Remote</title>
+</head>
+
+<body>
+    <h1>Hello world!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel tenetur, praesentium quas dignissimos sit soluta repudiandae aspernatur illo maxime reiciendis neque nobis culpa mollitia nisi quis nemo recusandae magni tempore?</p>
+</body>
+
+</html>
